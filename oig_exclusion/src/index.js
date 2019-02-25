e6cd07c8523b0aa571a132f82b57606d061ec230
CarlosElguedo <celguedo@condorlabs.io>
2019-02-25 16:02:53 -0500

    Updated process to save in file records

--- oig_exclusion/src/index.js
+++ oig_exclusion/src/index.js
@@ -4,7 +4,6 @@ let requiredEnv = ['VERIFICATION_PORT', 'ORA_CLIENT', 'ORA_USER', 'ORA_PASSWORD'
 
 for (let envVar of requiredEnv) {
 
-     console.log(process.env[envVar]);
     if (!process.env[envVar])
   
     throw new Error(`${envVar} env varible must be declared in .env file`);
